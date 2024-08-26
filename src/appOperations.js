51bc872c25b8899f334efe095ed48ee660f675df
Martin Harris <GitHub@BlackMael.com>
2024-08-27 09:45:51 +1200

    added divide feature

--- src/appOperations.js
+++ src/appOperations.js
@@ -10,8 +10,13 @@ function subtract(num1, num2) {
     return num1 - num2;
 }
 
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
 module.exports = {
     multiply,
     add,
-    subtract
+    subtract,
+    divide
 }
