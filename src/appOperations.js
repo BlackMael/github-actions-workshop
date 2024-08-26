4f96b1776151c6bb82909fc1058fe20975b8e9c1
Martin Harris <GitHub@BlackMael.com>
2024-08-27 10:00:01 +1200

    added wf to publish pkg and fixed add function

--- src/appOperations.js
+++ src/appOperations.js
@@ -3,7 +3,10 @@ function multiply(num1, num2) {
 }
 
 function add(num1, num2) {
-    return num1 + num2;
+    const intNum1 = parseInt(num1, 10);
+    const intNum2 = parseInt(num2, 10);
+
+    return intNum1 + intNum2;
 }
 
 function subtract(num1, num2) {
